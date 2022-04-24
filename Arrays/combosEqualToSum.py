5a4ae2ae3833774fbcdf7ba95215a0a32589b3cf
Ian Peck <ianpeck22@gmail.com>
2022-04-24 18:15:44 -0500

    combo pairs equal to a sum

--- Arrays/combosEqualToSum.py
+++ Arrays/combosEqualToSum.py
@@ -1,3 +1,13 @@
-# Given a list and a sum, find all combos of numbers that will add uop to it
+# Given a list and a sum, find all combo pairs of numbers that will add up to it
 
-def find_combos(mylist, target):
+def find_combos_pairs(mylist, target):
+    my_pairs = []
+    for i in range(len(mylist)//2):
+        if target - mylist[i] in mylist and mylist.index(target - mylist[i]) != i:
+            my_pairs.append([mylist[i], target - mylist[i]])
+    return my_pairs
+
+
+
+ 
+print(find_combos_pairs([1,2,3,4,5,6,7,8,9,10], 10))
