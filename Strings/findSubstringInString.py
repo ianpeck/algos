0ffc83e79336bf7da15f540d0f4ab4443e1fcb71
Ian Peck <ianpeck22@gmail.com>
2022-04-24 18:01:10 -0500

    Added comments

--- Strings/findSubstringInString.py
+++ Strings/findSubstringInString.py
@@ -1,11 +1,16 @@
 # Given a string, find the amount of times a substring occurs in the word
 
 def findSubstring(mystring,mysubstring):
+    # Iterates through a slice of string (whole thing in this case) and counts how many times the substring occurs
     return mystring.count(mysubstring, 0, len(mystring))
 
+
 def findSubstringIndices(mystring, mysubstring):
+    # Iterates the length of the string, checking if the string starts with our substring at each index
     result = [i for i in range(len(mystring)) if mystring.startswith(mysubstring,i) is True]
     return result
+    # Alternative way to do the first function (findSubstring) is to just return the lenth of this variable below.
+    return len(result)
 
 
 
