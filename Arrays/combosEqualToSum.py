f41d42f71fb7a1c2e81c147c7b05147a7d4b4bb0
Ian Peck <ianpeck22@gmail.com>
2022-04-25 12:26:28 -0500

    Changed sum logic and added hash map

--- Arrays/combosEqualToSum.py
+++ Arrays/combosEqualToSum.py
@@ -2,12 +2,15 @@
 
 def find_combos_pairs(mylist, target):
     my_pairs = []
+    hashmap = {}
     for i in range(len(mylist)//2):
-        if target - mylist[i] in mylist and mylist.index(target - mylist[i]) != i:
+        # if the target pair is in the list, the number is not the same index (the same 5 and 5 cannot add to 10), and the pair is not already in there
+        if target - mylist[i] in mylist and mylist.index(target - mylist[i]) != i and i not in hashmap: 
             my_pairs.append([mylist[i], target - mylist[i]])
+            hashmap[mylist.index(target - mylist[i])] = target - mylist[i]
     return my_pairs
 
 
 
  
-print(find_combos_pairs([1,2,3,4,5,6,7,8,9,10], 10))
+print(find_combos_pairs([1,2,3,4,5,6,7,8,9,10], 7))
